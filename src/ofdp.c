752dd0897ba765743fe0d421bff1b984737a373e
Aaron Bedra <aaron@aaronbedra.com>
2013-10-20 21:49:41 -0500

    Expire ofdp blacklist entries

--- src/ofdp.c
+++ src/ofdp.c
@@ -64,26 +64,38 @@ void ofdp_lookup_offenders(redisContext *context, config_t config)
 {
   int i, wafsec_score;
   char *address = malloc(16);
-  redisReply *offenders, *score, *noop;
+  redisReply *offenders, *score, *noop, *ttl;
 
   offenders = redisCommand(context, "ZRANGEBYSCORE offenders 0 +inf");
   if (offenders && (offenders->type == REDIS_REPLY_ARRAY)) {
     for (i = 0; i < offenders->elements; i++) {
       noop = redisCommand(context, "KEYS %s:repsheet:*", offenders->element[i]->str);
       if (noop && (noop->elements > 0)) {
-	freeReplyObject(noop);
-	continue;
+        freeReplyObject(noop);
+        continue;
       }
+
       score = redisCommand(context, "GET %s:score", offenders->element[i]->str);
       if (score && (score->type != REDIS_REPLY_NIL)) {
         freeReplyObject(score);
         continue;
       }
+
       wafsec_score = ofdp_score(ofdp_lookup(offenders->element[i]->str));
       redisCommand(context, "SET %s:score %d", offenders->element[i]->str, wafsec_score);
+
       if (wafsec_score > config.ofdp_threshold) {
+
         redisCommand(context, "SET %s:repsheet:blacklist true", offenders->element[i]->str);
         printf("Actor %s has been blacklisted due to high OFDP risk (Score: %d)\n", offenders->element[i]->str, wafsec_score);
+
+        ttl = redisCommand(context, "TTL %s:requests", offenders->element[i]->str);
+        if (ttl && ttl->integer > 0) {
+          redisCommand(context, "EXPIRE %s:repsheet:blacklist %d", offenders->element[i]->str, ttl->integer);
+          freeReplyObject(ttl);
+        } else {
+          redisCommand(context, "EXPIRE %s:repsheet:blacklist %d", offenders->element[i]->str, config.expiry);
+        }
       }
     }
     freeReplyObject(offenders);
