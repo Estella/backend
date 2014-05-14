a3578bf3e420994d58f188acff0555dc4483a4f7
Aaron Bedra <aaron@aaronbedra.com>
2014-05-14 12:22:53 -0500

    Make test runner output verbose

--- test/test_runner.c
+++ test/test_runner.c
@@ -6,7 +6,7 @@ int main(void) {
   srunner_add_suite(runner, make_analyze_suite());
   srunner_add_suite(runner, make_ofdp_suite());
   srunner_add_suite(runner, make_cli_suite());
-  srunner_run_all(runner, CK_NORMAL);
+  srunner_run_all(runner, CK_VERBOSE);
   int number_failed = srunner_ntests_failed(runner);
   srunner_free(runner);
 
